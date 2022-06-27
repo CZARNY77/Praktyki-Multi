6296e13c5d176410ae0d9a17bed90c533837e40e
CZARNY77 <60876248+CZARNY77@users.noreply.github.com>
2022-06-27 11:16:51 +0200

    3 -

--- Assets/Scripts/Luncher.cs
+++ Assets/Scripts/Luncher.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 namespace Com.MyCompany.MyGame
 {
-    public class Luncher : MonoBehaviour
+    public class Luncher : MonoBehaviourPunCallbacks
     {
         #region Private Serializable Fields
         #endregion
@@ -42,5 +43,34 @@ namespace Com.MyCompany.MyGame
             }
         }
         #endregion
+
+        #region MonoMegaviourPunCallbacks Callbacks
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+            //base.OnConnectedToMaster();
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+            //base.OnDisconnected(cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            PhotonNetwork.CreateRoom(null, new RoomOptions());
+            //base.OnJoinRandomFailed(returnCode, message);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+        #endregion
+
     }
 }
