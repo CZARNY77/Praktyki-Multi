c47c54e334d93e33977dae54c0127f5f41635a01
unknown <you@example.com>
2022-06-27 09:57:09 +0200

    2 -

---
+++ Assets/Scripts/Luncher.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.MyCompany.MyGame
+{
+    public class Luncher : MonoBehaviour
+    {
+        #region Private Serializable Fields
+        #endregion
+        #region Private Fields
+
+        string gameVersion = "1";
+
+        #endregion
+        #region MonoBehaviour CallBacks
+        void Awake()
+        {
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+        
+        void Start()
+        {
+            Connect();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+        #endregion
+    }
+}
